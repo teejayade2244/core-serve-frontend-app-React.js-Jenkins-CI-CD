bdbdc64d14d5ef1b79aeff7c8397e54821bad90d
TemitopeAde22 <temitope224468@gmail.com>
2025-05-31 13:37:27 +0100

    update

--- src/components/__tests__/Footer.test.jsx
+++ src/components/__tests__/Footer.test.jsx
@@ -10,11 +10,15 @@ describe("Footer Component", () => {
                 <Footer />
             </Router>
         )
-
         expect(screen.getByText(/Powered by Core serve/i)).toBeInTheDocument()
         expect(
             screen.getByText(/Copyright © 2025 Core Serve/i)
         ).toBeInTheDocument()
+        expect(screen.getByText(/All rights reserved/i)).toBeInTheDocument()
         expect(screen.getByText(/Privacy Policy/i)).toBeInTheDocument()
+
+        expect(
+            screen.getByRole("link", { name: /Powered by Core serve/i })
+        ).toHaveAttribute("href", "temitope")
     })
 })
