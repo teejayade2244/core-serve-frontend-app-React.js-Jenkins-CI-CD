43f07bc599f967b155833bbd71645b86d96e6ba1
ADEBUNMI TEMITOPE ADEDEJI <temitope224468@gmail.com>
2025-07-01 15:12:05 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -26,12 +26,12 @@ module.exports = {
         "^.+\\.(js|jsx)$": "babel-jest",
     },
     collectCoverageFrom: ["src/**/*.{js,jsx}", "!src/index.jsx"],
-    coverageThreshold: {
-        global: {
-            branches: 80,
-            functions: 80,
-            lines: 80,
-            statements: 80,
-        },
-    },
+    // coverageThreshold: {
+    //     global: {
+    //         branches: 80,
+    //         functions: 80,
+    //         lines: 80,
+    //         statements: 80,
+    //     },
+    // },
 }
